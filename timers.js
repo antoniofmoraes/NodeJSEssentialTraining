2d13fa0e53b8395522912128866c48ab10d00893
Antonio Felype Moraes <49417747+antoniofmoraes@users.noreply.github.com>
2022-01-07 09:26:39 -0300

    Chapter 3, lesson 7

--- timers.js
+++ timers.js
@@ -1,6 +1,18 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime/1000} seconds`);
+}
+
 console.log(`Setting a ${waitTime / 1000} seconds delay`);
 
-const timeout = () => {console.log("done")}; 
+const timeout = () => {
+    clearInterval(intervals);
+    console.log("done");
+}; 
 
+const intervals = setInterval(incTime, waitInterval);
 setTimeout(timeout, waitTime);
