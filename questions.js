3221f8fa2660acc4d36959bb9526c578e9b78615
Antonio Felype Moraes <49417747+antoniofmoraes@users.noreply.github.com>
2022-01-11 11:17:30 -0300

    Chapter 4, lesson 5

--- questions.js
+++ questions.js
@@ -1,32 +1,10 @@
-const readLine = require('readline');
-
-const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+const collectAnswers = require('./lib/collectAnswers.js');
 
 const questions = [
     "What is your name? ",
     "Where do you live? ",
     "What are you going to do with node.js? "
 ];
-
-const collectAnswers = (questions, done) => {
-    const answers = [];
-    const [firstQuestion] = questions;
-    //done(answers)
-
-    const questionAnswered = answer => {
-        answers.push(answer);
-        if (answers.length < questions.length) {
-            rl.question(questions[answers.length], questionAnswered);
-        } else {
-            done(answers);
-        }
-    }
-
-    rl.question(firstQuestion, questionAnswered);
-}
 
 collectAnswers(questions, answers => {
     console.log("Thank you for your answers");
